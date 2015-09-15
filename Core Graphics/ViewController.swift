d68b27ce2c587167fbf3162e510c2050412717bc
engjasonp <engjasonp@gmail.com>
2015-09-14 18:33:41 -0700

    Added mouse.png to supporting files. Added UIImageView named imageView to VC and action button beneath it named redrawTapped(). Added appropriate constraints.

--- Core Graphics/ViewController.swift
+++ Core Graphics/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func redrawTapped(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
