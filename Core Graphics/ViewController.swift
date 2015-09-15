65ebdfa13a70e15e200300743f55b9f4572a4110
engjasonp <engjasonp@gmail.com>
2015-09-14 18:39:43 -0700

    Added property currentDrawType = 0 to keep track of current draw type. Added code to IBAction redrawTapped to ++currentDrawType and reset to 0 once it hits past 5. Default draw type in viewDidLoad is drawRectangle().

--- Core Graphics/ViewController.swift
+++ Core Graphics/ViewController.swift
@@ -12,11 +12,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func redrawTapped(sender: AnyObject) {
+        ++currentDrawType
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+            
+        default:
+            break
+        }
     }
+    
+    var currentDrawType = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        drawRectangle()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +40,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func drawRectangle() {
+        
+    }
 }
 
